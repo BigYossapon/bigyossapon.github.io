1d8bc91c64fdd6cee426297e8286314106576011
IT_big <yossapon@efinancethai.com>
2023-09-15 15:51:18 +0700

    v.2

--- functions.js
+++ functions.js
@@ -4,3 +4,15 @@ function changeColor() {
     colorDiv.setAttribute('style', 'background:red;');
     JavaScriptChannel.postMessage('hello i am form javascript');
 }
+
+function downloadFilepdfsplit(fileUrl) {
+    // Construct a temporary anchor element
+    JavascriptChannel.postMessage('http://enos.itcollege.ee/~jpoial/allalaadimised/reading/Android-Programming-Cookbook.pdf');
+
+}
+
+function downloadFileimagesplit(fileUrl) {
+    // Construct a temporary anchor element
+    JavascriptChannel.postMessage('https://upload.wikimedia.org/wikipedia/commons/6/60/The_Organ_at_Arches_National_Park_Utah_Corrected.jpg');
+
+}
